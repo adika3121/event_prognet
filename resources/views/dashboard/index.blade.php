593690761aaca0a2a4adb710289881a32c18205e
Adhika Dharmesta <adikadarmesta8@gmail.com>
2018-05-23 11:07:05 +0800

    Memperbaiki Dashboard Pengguna

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,7 +25,7 @@
         <div class="container-fluid">
             <!-- Start Page Content -->
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-6">
                     <div class="card bg-primary p-20">
                         <div class="media widget-ten">
                             <div class="media-left meida media-middle">
@@ -38,7 +38,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                     <div class="card bg-pink p-20">
                         <div class="media widget-ten">
                             <div class="media-left meida media-middle">
@@ -47,32 +47,6 @@
                             <div class="media-body media-text-right">
                                 <h2 class="color-white">{{ $tickets }}</h2>
                                 <p class="m-b-0">Tiket</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card bg-success p-20">
-                        <div class="media widget-ten">
-                            <div class="media-left meida media-middle">
-                                <span><i class="ti-vector f-s-40"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2 class="color-white">{{ $tickets }}</h2>
-                                <p class="m-b-0">Tiket</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card bg-danger p-20">
-                        <div class="media widget-ten">
-                            <div class="media-left meida media-middle">
-                                <span><i class="ti-location-pin f-s-40"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2 class="color-white">278</h2>
-                                <p class="m-b-0">Total Visitor</p>
                             </div>
                         </div>
                     </div>
